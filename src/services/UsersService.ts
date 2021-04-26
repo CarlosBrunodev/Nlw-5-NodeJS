40a5270e317b01fd3706fcc4ae74852402dfa944
CarlosBrunodev <carlosbruno.dev@gmail.com>
2021-04-26 12:30:03 -0300

    Iniciando o projeto

---
+++ src/services/UsersService.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository , Repository } from 'typeorm';
+import { Users } from '../entities/Users';
+import { UserRepository } from '../repositories/UserRepository';
+
+
+class UsersService {
+
+    private usersRepository : Repository<Users>
+
+    constructor(){
+        this.usersRepository = getCustomRepository(UserRepository)
+    }
+    async create( email: string){
+
+        const userExists = await this.usersRepository.findOne({
+            email 
+        })
+
+        if(userExists){
+            return userExists
+        }
+
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+    }
+
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({ email });
+    
+        return user;
+      }
+}
+
+export { UsersService }
